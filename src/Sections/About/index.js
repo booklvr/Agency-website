f2199633074a0aaaf3a49c0a78cb29b666a00991
nicholas de waal <nick.a.dewaal@gmail.com>
2021-06-08 16:28:22 +0900

    finished hero section start about

--- src/Sections/About/index.js
+++ src/Sections/About/index.js
@@ -1,7 +1,23 @@
 // This is About component, It will contain about us info.
+import styled, { keyframes } from 'styled-components'
+import Hand from '../../assets/hand.svg'
+import Wave from '../../assets/waves.svg'
+
+const AboutSection = styled.div``
+
+const Waves = styled.img``
+
+const HandContainer = styled.div``
 
 const About = () => {
-  return <div>About Component</div>;
-};
+  return (
+    <AboutSection>
+      <Waves src={Wave} alt='waves' width='400' />
+      <HandContainer>
+        <img src={Hand} alt='hand' width='400' />
+      </HandContainer>
+    </AboutSection>
+  )
+}
 
-export default About;
+export default About
--- src/Sections/Hero/index.js
+++ src/Sections/Hero/index.js
@@ -1,23 +1,45 @@
 // This is HeroSection component, Main top section of website
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 import Pink from '../../assets/blobPink.png'
 import White from '../../assets/blobWhite.png'
 import Purple from '../../assets/blobPurple.png'
+import Arrow from '../../assets/arrowRight.svg'
+import Mobile from '../../assets/mobile.svg'
+
+const move = keyframes`
+  0% { transform: translateY(-5px); }
+  50% { transform: translateY(10px); }
+  100% { transform: translateY(-5px); }
+`
 
 const HeroSectionContainer = styled.div`
   width: 100vw;
   height: 45vw;
   background-color: var(--black);
-
+  color: var(--white);
   display: flex;
   justify-content: center;
   position: relative;
+
+  @media only Screen and (max-width: 48em) {
+    height: 70vw;
+    display: block;
+  }
+
+  @media only Screen and (max-width: 420px) {
+    height: auto;
+    padding-bottom: 2rem;
+  }
 `
 
 const Blobs = styled.div`
   width: 100%;
   position: absolute;
   right: 0;
+
+  @media only Screen and (max-width: 48em) {
+    opacity: 0.5;
+  }
 `
 
 const PinkBlob = styled.div`
@@ -42,6 +64,114 @@ const PurpleBlob = styled.div`
   right: 0;
 `
 
+const MainContent = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 70vw;
+
+  @media only Screen and (max-width: 48em) {
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100vw;
+  }
+`
+
+const LeftBlock = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  width: 50%;
+  line-height: 1.5;
+
+  @media only Screen and (max-width: 48em) {
+    width: 80%;
+    text-align: center;
+    align-items: center;
+    justify-content: space-around;
+    margin-top: calc(2.5rem + 2.5vw);
+    filter: drop-shadow(2px 4px 6px var(--black));
+  }
+
+  
+`
+
+const Topic = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: var(--nav);
+  font-weight: 700;
+  font-size: calc(0.4rem + 0.4vw);
+  padding: 0.5rem 1rem;
+  border-radius: 20px;
+`
+
+const Circle = styled.span`
+  display: inline-block;
+  width: 1rem;
+  height: 1rem;
+  border-radius: 50%;
+  background-color: var(--purple);
+  margin-right: 0.5rem;
+`
+
+const Title = styled.h1`
+  font-size: calc(2rem + 1vw);
+  line-height: 1.2;
+  padding: 0.5rem 0;
+`
+
+const Subtext = styled.h5`
+  font-size: calc(0.5rem + 0.5vw);
+  color: var(--nav2);
+`
+
+const CTA = styled.button`
+  padding: 0.5rem 1rem;
+  margin-top: 1rem;
+  border-radius: 20px;
+  cursor: pointer;
+  font-size: calc(0.5rem + 0.5vw);
+  font-weight: 700;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: all 0.2s;
+
+  &:hover {
+    transform: scale(1.1);
+  }
+
+  &:active {
+    transform: scale(0.9);
+  }
+
+  img {
+    width: 1.5rem;
+  }
+`
+
+const MobileSvg = styled.img`
+  max-width: 100%;
+  width: calc(30% + 20vw);
+  z-index: 7;
+  height: auto;
+  animation: ${move} 2.5s ease infinite;
+
+  @media only Screen and (max-width: 48em) {
+    align-self: flex-start;
+    position: absolute;
+    bottom: 0;
+    opacity: 0.5;
+  }
+
+  @media only screen and (max-width: 40em) {
+    display: none;
+  }
+`
+
 const HeroSection = () => {
   return (
     <HeroSectionContainer>
@@ -56,7 +186,21 @@ const HeroSection = () => {
           <img src={Purple} alt='purple blob' width='400' />
         </PurpleBlob>
       </Blobs>
-      HeroSection Component
+      <MainContent>
+        <LeftBlock>
+          <Topic>
+            <Circle /> <span>We Build Web</span>
+          </Topic>
+          <Title>Transforming your digital presence</Title>
+          <Subtext>
+            We help fast growing companies build award winning websites
+          </Subtext>
+          <CTA>
+            Get in touch &nbsp; <img src={Arrow} alt='CTA' width='400' />
+          </CTA>
+        </LeftBlock>
+        <MobileSvg src={Mobile} alt='Mobile Svg' width='400' />
+      </MainContent>
     </HeroSectionContainer>
   )
 }
